d71e83bfa53acaa3eab7bfbdce5dfdfdd6f20255
Dang <huynhminhdang@gmail.com>
2022-11-24 04:37:53 +0700

    #1: fix interrupted system call in iTerm
    Closes #1

--- conf.d/startup-dir.fish
+++ conf.d/startup-dir.fish
@@ -9,8 +9,8 @@ if test "$PWD" = "$HOME" -a -n "$startup_dir"
   while test ! -d "$startup_dir"
     set startup_dir (dirname $startup_dir)
   end
-  builtin cd $startup_dir >/dev/null 2>&1
   if test "$TERM_PROGRAM" = "iTerm.app"
-    builtin cd $startup_dir >/dev/null 2>&1
+    fish -c "builtin cd" >/dev/null 2>&1
   end
+  builtin cd $startup_dir >/dev/null 2>&1
 end
