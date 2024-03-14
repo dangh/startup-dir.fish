54b8f07d146d7c9d4cefb37d5ca6417262bbd6ec
Dang <huynhminhdang@gmail.com>
2024-03-14 08:16:55 +0800

    format code with fish_indent

--- conf.d/startup-dir.fish
+++ conf.d/startup-dir.fish
@@ -1,16 +1,16 @@
 status is-interactive || exit
 
 function set-startup-dir -a dir
-  test -n "$dir" || set -l dir "$PWD"
-  test -d "$dir" && set -U startup_dir (realpath $dir)
+    test -n "$dir" || set -l dir "$PWD"
+    test -d "$dir" && set -U startup_dir (realpath $dir)
 end
 
 if test "$PWD" = "$HOME" -a -n "$startup_dir"
-  while test ! -d "$startup_dir"
-    set startup_dir (path dirname $startup_dir)
-  end
-  if test "$TERM_PROGRAM" = "iTerm.app"
-    fish -c "builtin cd" >/dev/null 2>&1
-  end
-  builtin cd $startup_dir >/dev/null 2>&1
+    while test ! -d "$startup_dir"
+        set startup_dir (path dirname $startup_dir)
+    end
+    if test "$TERM_PROGRAM" = "iTerm.app"
+        fish -c "builtin cd" >/dev/null 2>&1
+    end
+    builtin cd $startup_dir >/dev/null 2>&1
 end
